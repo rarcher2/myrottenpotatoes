ef882e46524bbb74a489788f935cb4eacfad6977
Robert <rarcher2@uncc.edu>
2014-10-12 13:37:00 -0300

    initial commit

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ActiveRecord::Base
+	attr_accessible :title, :rating, :description, :release_date
+	def self.all_ratings
+		['G','PG','PG-13','R','NC-17']
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '180870a1419dbfdcfe65daef2952d4cfc12fb3c41626d439e14c1fa375e20477c352977792b699eb149b20f738a6d017b8d875b465d6ccbf5b0cb17f702b32a9'
